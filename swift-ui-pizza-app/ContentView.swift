1635bd0f17586b4284ec77832b555a7beca0a6c3
Eoan O Dea <odeaeoan@gmail.com>
2020-03-12 14:31:11 +0000

    Initial Commit

---
+++ swift-ui-pizza-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-ui-pizza-app
+//
+//  Created by Eoan on 12/03/2020.
+//  Copyright © 2020 WebSpace. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
